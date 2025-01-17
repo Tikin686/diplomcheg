bca68f9f9223e9e6c91c2d14e56863cdf56129d4
Tikin686 <sergey.tikin94@gmail.com>
2025-01-18 01:23:25 +0300

    init

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from restaurants.forms import RestaurantForm
+from restaurants.models import Restaurant
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class RestaurantListView(ListView):
+    model = Restaurant
+    template_name = "restaurants_list.html"
+
+
+class RestaurantCreateView(CreateView):
+    model = Restaurant
+    form_class = RestaurantForm
+    success_url = reverse_lazy("restaurant_list")
+
+
+class RestaurantDetailView(DetailView):
+    model = Restaurant
+    template_name = "restaurant_detail.html"
+
+
+class RestaurantUpdateView(UpdateView):
+    model = Restaurant
+    form_class = RestaurantForm
+    success_url = reverse_lazy("restaurant_list")
+
+
+class RestaurantDeleteView(DeleteView):
+    model = Restaurant
+    template_name = "restaurant_confirm_delete.html"
+    success_url = reverse_lazy("restaurant_list")
+
+
+def index(request):
+    return render(request, template_name='base.html')
