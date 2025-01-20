d99da0b1a0f7ac3a9feb7e6ca2b7b188c086c4b4
Tikin686 <sergey.tikin94@gmail.com>
2025-01-20 19:48:53 +0300

    fix

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 
+NULLABLE = {"null": True, "blank": True}
+
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название', help_text='Напишите название ресторана')
     phone = models.CharField(max_length=15, verbose_name='Номер телефона', help_text='Введите номер ресторана')
     address = models.CharField(max_length=250, verbose_name='Адрес', help_text='Введите адрес ресторана')
     description = models.TextField(verbose_name='Описание ресторана')
-    history = models.TextField(verbose_name='История ресторана', null=True, blank=True)
-    mission = models.TextField(verbose_name='Миссия ресторана', null=True, blank=True)
-    command = models.TextField(verbose_name='Команда', null=True, blank=True)
+    history = models.TextField(verbose_name='История ресторана', **NULLABLE)
+    mission = models.TextField(verbose_name='Миссия ресторана', **NULLABLE)
+    command = models.TextField(verbose_name='Команда', **NULLABLE)
     services = models.TextField(verbose_name='Предоставляемые услуги')
     total_seats = models.PositiveIntegerField(verbose_name='Общее количество мест')
     free_seats = models.PositiveIntegerField(verbose_name='Количество свободных мест')
-    reserved_seats = models.PositiveIntegerField(verbose_name='Количество занятых мест')
+    reserved_seats = models.PositiveIntegerField(verbose_name='Количество занятых мест', null=True, blank=True)
     photo = models.ImageField(upload_to='restaurant/photo', blank=True, null=True, verbose_name='Фото', help_text='Загрузите фото ресторана')
 
     def __str__(self):
