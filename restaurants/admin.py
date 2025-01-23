65a1c2a4f080c3d903dddb2dfae37ee247f9b5ee
Tikin686 <sergey.tikin94@gmail.com>
2025-01-23 20:10:29 +0300

    add @admin.register(Table)

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from restaurants.models import Restaurant, Reserve
+from restaurants.models import Restaurant, Reserve, Table
 
 
 @admin.register(Restaurant)
@@ -9,8 +9,13 @@ class RestaurantAdmin(admin.ModelAdmin):
     search_fields = ('name', 'address')
 
 
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('number', 'size')
+    list_filter = ('number', )
+
+
 @admin.register(Reserve)
 class ReserveAdmin(admin.ModelAdmin):
-    list_display = ('name', 'address', 'description')
+    list_display = ('name', 'number', 'start_reserve', 'end_reserve')
     list_filter = ('name', )
-    search_fields = ('name', 'address')
