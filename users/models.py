176c3bd9a31c2740e9b83d3f853690bedb1f80df
Tikin686 <sergey.tikin94@gmail.com>
2025-01-23 20:11:08 +0300

    fix

--- users/models.py
+++ users/models.py
@@ -5,8 +5,11 @@ NULLABLE = {"null": True, "blank": True}
 
 
 class User(AbstractUser):
+    """
+    Пользователь
+    """
     username = None
-    email = models.EmailField(verbose_name='Ваша почта',unique=True)
+    email = models.EmailField(verbose_name='Ваша почта', unique=True)
     first_name = models.TextField(verbose_name='Введите имя', max_length=50)
     last_name = models.TextField(verbose_name='Введите фамилию', max_length=50)
     phone = models.CharField(max_length=20, verbose_name='Номер телефона')
@@ -20,4 +23,7 @@ class User(AbstractUser):
         verbose_name_plural = "Пользователи"
 
     def __str__(self):
-        return self.email
+        return f'{self.first_name}{self.last_name}'
+
+    def __repr__(self):
+        return f'{self.email}'
