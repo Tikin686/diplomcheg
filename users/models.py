5d81dc4929f03c886a247805646960a5d5e2fa48
Tikin686 <sergey.tikin94@gmail.com>
2025-01-23 17:35:30 +0300

    fix

--- users/models.py
+++ users/models.py
@@ -6,11 +6,14 @@ NULLABLE = {"null": True, "blank": True}
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True)
+    email = models.EmailField(verbose_name='Ваша почта',unique=True)
     first_name = models.TextField(verbose_name='Введите имя', max_length=50)
     last_name = models.TextField(verbose_name='Введите фамилию', max_length=50)
-    phone = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20, verbose_name='Номер телефона')
     token = models.CharField(max_length=100, verbose_name='Токен', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = "Пользователь"
