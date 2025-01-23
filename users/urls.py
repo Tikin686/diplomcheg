8893058ea00b38cd7941e430abe4bd7c289f4ca5
Tikin686 <sergey.tikin94@gmail.com>
2025-01-23 17:37:10 +0300

    init

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, reverse_lazy
+from users.apps import UsersConfig
+from users.views import UserCreateView, UserListView, UserUpdateView
+
+app_name = UsersConfig.name
+
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name="users/login.html"), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', UserCreateView.as_view(), name='register'),
+    path("users/", UserListView.as_view(), name="user_list"),
+    path("users/<int:pk>/update/", UserUpdateView.as_view(), name="user_update"),
+]
