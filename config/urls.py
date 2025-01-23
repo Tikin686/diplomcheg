f9beacfcb962407961bf39989a706a30ebaac110
Tikin686 <sergey.tikin94@gmail.com>
2025-01-23 17:36:49 +0300

    fix

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('restaurants.urls', namespace='restaurants'))
+    path('', include('restaurants.urls', namespace='restaurants')),
+    path("users/", include("users.urls", namespace="users")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
