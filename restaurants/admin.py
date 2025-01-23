92b1da808f84c8d55117e86e030d6b44c56ab444
Tikin686 <sergey.tikin94@gmail.com>
2025-01-23 17:33:06 +0300

    fix

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from restaurants.models import Restaurant
+from restaurants.models import Restaurant, Reserve
 
 
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'description')
+    list_filter = ('name', )
+    search_fields = ('name', 'address')
+
+
+@admin.register(Reserve)
+class ReserveAdmin(admin.ModelAdmin):
     list_display = ('name', 'address', 'description')
     list_filter = ('name', )
     search_fields = ('name', 'address')
