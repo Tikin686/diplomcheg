1c19669c61eb9e5ac9956942ae3c853a70ba3211
Tikin686 <sergey.tikin94@gmail.com>
2025-01-29 22:57:51 +0300

    fix

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -5,17 +5,13 @@ from restaurants.models import Restaurant, Reserve, Table
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
     list_display = ('name', 'address', 'description')
-    list_filter = ('name', )
-    search_fields = ('name', 'address')
 
 
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
-    list_display = ('number', 'size')
-    list_filter = ('number', )
+    list_display = ('number', 'seats')
 
 
 @admin.register(Reserve)
 class ReserveAdmin(admin.ModelAdmin):
-    list_display = ('name', 'number', 'start_reserve', 'end_reserve')
-    list_filter = ('name', )
+    list_display = ('client',)
