b729ba45b5b0ddb16847dd88c5a30afd9cf01707
Tikin686 <sergey.tikin94@gmail.com>
2025-01-31 18:15:31 +0300

    fix

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,18 +1,35 @@
-from restaurants.apps import RestaurantsConfig
 from django.urls import path
-from restaurants.views import RestaurantDetailView, RestaurantListView, RestaurantCreateView, RestaurantUpdateView, \
-    RestaurantDeleteView, ReserveCreateView, ReserveListView, ReserveCancelView, ReserveUpdateView
+from django.views.generic import TemplateView
+
+from restaurants.apps import RestaurantsConfig
+from restaurants.views import (FeedbackCreateView, ReserveCancelView,
+                               ReserveCreateView, ReserveListView,
+                               ReserveUpdateView, RestaurantCreateView,
+                               RestaurantDeleteView, RestaurantDetailView,
+                               RestaurantListView, RestaurantUpdateView)
 
 app_name = RestaurantsConfig.name
 
 urlpatterns = [
-    path('', RestaurantListView.as_view(), name='restaurant_list'),
-    path('create/', RestaurantCreateView.as_view(), name='restaurant_create'),
-    path('<int:pk>/update/', RestaurantUpdateView.as_view(), name='restaurant_update'),
-    path('<int:pk>/', RestaurantDetailView.as_view(), name='restaurant_detail'),
-    path('<int:pk>/delete/', RestaurantDeleteView.as_view(), name='restaurant_delete'),
-    path('<int:pk>/reserve/create/', ReserveCreateView.as_view(), name='reserve_create'),
-    path('reserve/', ReserveListView.as_view(), name='reserve_list'),
-    path('reserve/<int:pk>/cancel/', ReserveCancelView.as_view(), name='reserve_cancel'),
-    path('reserve/<int:pk>/update/', ReserveUpdateView.as_view(), name='reserve_update'),
+    path("", RestaurantListView.as_view(), name="restaurant_list"),
+    path("create/", RestaurantCreateView.as_view(), name="restaurant_create"),
+    path("<int:pk>/update/", RestaurantUpdateView.as_view(), name="restaurant_update"),
+    path("<int:pk>/", RestaurantDetailView.as_view(), name="restaurant_detail"),
+    path("<int:pk>/delete/", RestaurantDeleteView.as_view(), name="restaurant_delete"),
+    path(
+        "<int:pk>/reserve/create/", ReserveCreateView.as_view(), name="reserve_create"
+    ),
+    path("reserve/", ReserveListView.as_view(), name="reserve_list"),
+    path(
+        "reserve/<int:pk>/cancel/", ReserveCancelView.as_view(), name="reserve_cancel"
+    ),
+    path(
+        "reserve/<int:pk>/update/", ReserveUpdateView.as_view(), name="reserve_update"
+    ),
+    path("feedback/<int:pk>/", FeedbackCreateView.as_view(), name="feedback"),
+    path(
+        "feedback/success/",
+        TemplateView.as_view(template_name="restaurants/feedback_success.html"),
+        name="feedback_success",
+    ),
 ]
